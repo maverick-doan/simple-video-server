25861cff111844b0dc85fd8040e0c1c173420052
Maverick Doan <sharkysondoan@gmail.com>
2025-08-10 15:48:09 +1000

    feat(jwt): Added jwt signing and verification utilities

---
+++ src/utils/jwt.ts
@@ -0,0 +1,12 @@
+import * as jwt from 'jsonwebtoken';
+import { env } from '../config/env';
+import type { JwtUser } from '../types/user';
+import type { Secret, SignOptions } from 'jsonwebtoken';
+
+export function signJwt(user: JwtUser, expiresIn: string = '1h') {
+    return jwt.sign(user, env.jwtSecret as Secret, { expiresIn } as SignOptions);
+}
+
+export function verifyJwt(token: string): JwtUser {
+    return jwt.verify(token, env.jwtSecret as Secret) as JwtUser;
+}
