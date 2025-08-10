c0688c4eee6a64d99de8c5ab27fb113fc255cb37
Maverick Doan <sharkysondoan@gmail.com>
2025-08-10 17:04:15 +1000

    fix: Fixed broken import for app binding after refactor

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,7 @@
-import { type JwtUser } from "./user";
+import { type JwtUser } from "../types/user";
 
+// This adds type safety to the context object 
+// Also to avoid unexpected type errors that TS compiler would scream at you
 export type AppBindings = {
     user?: JwtUser;
 }
