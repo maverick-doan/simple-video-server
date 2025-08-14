45e4bd73292732ec556000635cb18f9005012f99
Maverick Doan <sharkysondoan@gmail.com>
2025-08-14 16:38:17 +1000

    fix(database): Fixed bad password seeding

--- sql/001__seed__default_user.sql
+++ sql/001__seed__default_user.sql
@@ -7,7 +7,7 @@ VALUES (
     'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
     'admin',
     'admin@superman.com',
-    '$2a$10$rK7PXqYEoaXZ.gGQiPM1xOLGe0sGzFZ7QUdL3aNbGxFZGtLWYqQZi' -- 'admin123' x10
+    '$2b$10$ThtO27Y9o1di6wBB7WLf/u.2zvRMs.kr9QxXry9t3alWjOE/thlqe' -- 'admin123' x10
 ) ON CONFLICT (email) DO NOTHING;
 
 INSERT INTO users (id, username, email, password_hash)
@@ -15,7 +15,7 @@ VALUES (
     'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12',
     'user',
     'user@superman.com',
-    '$2a$10$YmxMGgA9V3ZNqMJ8lxHpkuXY.kC.M8RHQhU4mWDO.YrqFxFzXpQPO' -- user123 x10
+    '$2b$10$DB/zVFa87Fn60xIwfhYQy.kdHwjgS0BGZ6UIHiGNBKRayLM8UAz1u' -- user123 x10
 ) ON CONFLICT (email) DO NOTHING;
 
 COMMIT;
