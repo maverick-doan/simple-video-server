5d82f942320bb74984ebabf9bde3bbcfabbac009
Maverick Doan <sharkysondoan@gmail.com>
2025-08-16 22:15:49 +1000

    fix: Added missing route for logout

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { login, me } from "../controllers/authController";
+import { login, me, logout } from "../controllers/authController";
 import { requireAuth } from "../middleware/authc";
 import type { AppBindings } from "../config/app";
 
@@ -7,3 +7,4 @@ export const authRouter = new Hono<{ Variables: AppBindings }>();
 
 authRouter.post('/login', login);
 authRouter.get('/me', requireAuth, me);
+authRouter.post('/logout', requireAuth, logout);
