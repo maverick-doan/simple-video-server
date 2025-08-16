7d16a5e1ae0b38a75c00fada212b8266147143fe
Maverick Doan <sharkysondoan@gmail.com>
2025-08-16 23:54:58 +1000

    fix: Fixed user authorisation
    Missing adminin allowed users

--- src/routes/video.ts
+++ src/routes/video.ts
@@ -7,7 +7,7 @@ import { requestTranscodeJob, getTranscodeJob } from "../controllers/transcodeJo
 
 export const videoRouter = new Hono<{ Variables: AppBindings }>();
 
-videoRouter.get('/:id', requireAuth, requireRole(['user']), getVideo);
-videoRouter.post('/upload', requireAuth, requireRole(['user']), uploadVideo);
-videoRouter.post('/transcode', requireAuth, requireRole(['user']), requestTranscodeJob);
-videoRouter.get('/transcode/:id', requireAuth, requireRole(['user']), getTranscodeJob);
+videoRouter.get('/:id', requireAuth, requireRole(['user', 'admin']), getVideo);
+videoRouter.post('/upload', requireAuth, requireRole(['user', 'admin']), uploadVideo);
+videoRouter.post('/transcode', requireAuth, requireRole(['user', 'admin']), requestTranscodeJob);
+videoRouter.get('/transcode/:id', requireAuth, requireRole(['user', 'admin']), getTranscodeJob);
