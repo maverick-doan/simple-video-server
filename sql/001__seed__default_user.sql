2b34541b1f3f3e804f86645c3e0df9e64946d22a
Maverick Doan <sharkysondoan@gmail.com>
2025-08-17 02:38:39 +1000

    fix: Added missing user roles

--- sql/001__seed__default_user.sql
+++ sql/001__seed__default_user.sql
@@ -2,20 +2,22 @@
 
 BEGIN;
 
-INSERT INTO users (id, username, email, password_hash)
+INSERT INTO users (id, username, email, password_hash, role)
 VALUES (
     'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
     'admin',
     'admin@superman.com',
-    '$2b$10$ThtO27Y9o1di6wBB7WLf/u.2zvRMs.kr9QxXry9t3alWjOE/thlqe' -- 'admin123' x10
+    '$2b$10$ThtO27Y9o1di6wBB7WLf/u.2zvRMs.kr9QxXry9t3alWjOE/thlqe', -- 'admin123' x10
+    'admin'
 ) ON CONFLICT (email) DO NOTHING;
 
-INSERT INTO users (id, username, email, password_hash)
+INSERT INTO users (id, username, email, password_hash, role)
 VALUES (
     'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12',
     'user',
     'user@superman.com',
-    '$2b$10$DB/zVFa87Fn60xIwfhYQy.kdHwjgS0BGZ6UIHiGNBKRayLM8UAz1u' -- user123 x10
+    '$2b$10$DB/zVFa87Fn60xIwfhYQy.kdHwjgS0BGZ6UIHiGNBKRayLM8UAz1u', -- user123 x10
+    'user'
 ) ON CONFLICT (email) DO NOTHING;
 
 COMMIT;
